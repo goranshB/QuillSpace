fe117adb2d6d7c2d56eda08141899c0baf1b7b0c
GoranshB <121124008+goranshB@users.noreply.github.com>
2025-08-04 01:46:33 +0530

    Update db.js
    changes has been made in db.js to work woth supabase

--- db.js
+++ db.js
@@ -1,12 +1,23 @@
+// import pkg from "pg";
+// const { Pool } = pkg;
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   password: "database_postgre",
+//   port: 5432,
+//   database: "Project-ONE",
+// });
+
+// export default pool;
+
 import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  password: "database_postgre",
-  port: 5432,
-  database: "Project-ONE",
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, 
 });
 
 export default pool;
+
